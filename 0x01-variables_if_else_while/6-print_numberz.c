8acdb376e1cab5a88fef4e0e47a9390845987722
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-20 16:32:34 -0700

    6-print_numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+{
+		putchar(y + 48);
+}
+	putchar('\n');
+
+	return (0);
+}
