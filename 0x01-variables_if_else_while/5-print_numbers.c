1ab16951f038bc02a145dafc4057be6908b1e035
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-20 16:15:22 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always zero for success
+ * Description: prints all single number of base 10
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x  <= 9; x++)
+{
+		printf("%d", x);
+}
+	printf("\n");
+
+	return (0);
+}
