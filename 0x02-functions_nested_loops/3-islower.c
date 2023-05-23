d78d39a20f3a5e9359477f0ed1e00157b70a3ce3
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-23 16:36:57 -0700

    islower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Checks for lower case character
+ * Return: 1 for lowecase or 0 for anything
+ * @c: Character to be printed
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+{
+		return (1);
+}
+		return (0);
+}
