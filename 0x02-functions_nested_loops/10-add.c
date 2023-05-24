66e887bdb5d9fe0b5d57917334335f28acceba5f
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-24 07:41:53 -0700

    add to number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Sum two number
+ * @n1: One of the number to be sum
+ * @n2: Other number
+ * Return: Result
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
