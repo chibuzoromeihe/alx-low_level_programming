e427a90477bacba99ae52fe15fa67d09c5d5a2b6
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-28 07:15:17 -0700

    Multiples two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 intergers
+ * @a: One interger
+ * @b: Another interger
+ * Return: Returns result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
