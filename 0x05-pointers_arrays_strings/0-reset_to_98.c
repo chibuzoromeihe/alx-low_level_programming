6ff74b7fb8caa40cbfa24e263017d078e0901039
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-29 15:56:52 -0700

    pointer to ab int as parameter and updates the value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as a parameter
+ * @n: Input number
+ * Return: Returns n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project focuse on pointers, arrays, and strings
