a513aa7a7553b040c2b57b72524721fe4ad9f309
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-05-30 05:21:29 -0700

    Prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints all other characters of a string
+ * @str: String
+ * Return: Always zero
+ */
+
+void puts2(char *str)
+{
+	int x;
+	int y = 0;
+
+	while (str[y] != '\0')
+	{
+		y++;
+	}
+	for (x = 0; x < y; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
