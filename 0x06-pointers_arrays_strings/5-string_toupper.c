c9ab04c23a4e407b0e6b5d653949b19740ea5002
chibuzor omeihe <corneliusomeihe@gmail.com>
2023-06-01 07:09:36 -0700

    Changes all lowercase of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes lowercase string to uppercase
+ * @x: String pointer
+ * Return: Pointer to uppercase
+ */
+
+char *string_toupper(char *x)
+{
+	int string_lenght = 0;
+
+	while (x[string_lenght] != '\0')
+	{
+		if (x[string_lenght] > 97 && x[string_lenght] <= 122)
+		{
+			x[string_lenght] = x[string_lenght] - 32;
+		}
+		string_lenght++;
+	}
+	return (x);
+}
